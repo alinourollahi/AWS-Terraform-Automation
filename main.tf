9ceb62d3ddaec5278f33f811de52af6e64ecef61
ali.nourollahi <alinourollahi777@gmail.com>
2025-06-08 14:32:03 +0330

    Run Nginx docker container inside EC2 instance

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable avail_zone {}
 variable env_prefix {}
 variable my-ip {}
 variable instance_type {}
+variable public_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -107,11 +108,28 @@ resource "aws_instance" "myapp-server" {
     availability_zone = var.avail_zone
 
     associate_public_ip_address = true
-    key_name = "my-server-key"
+    key_name = aws_key_pair.ssh-key.key_name
+
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo yum install -y docker
+                sudo systemctl start docker
+                sudo systemctl enable docker
+                sudo usermod -aG docker ec2-user
+                docker run -d -p 8080:80 nginx
+                EOF
 
     tags = {
         Name: "${var.env_prefix}-server"
     }
+}
+
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = "${file(var.public_key_location)}"
 }
 
 output "myserver-public-ip" {
